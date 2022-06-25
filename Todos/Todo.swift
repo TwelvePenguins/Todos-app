07ad871c1690e8da9ee72fcd8609ecd82ebd9ae2
Du Yuhan <yuhan.du@gmail.com>
2022-06-25 10:31:47 +0800

    Added todo struct

---
+++ Todos/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todos
+//
+//  Created by Yuhan Du on 25/6/22.
+//
+
+import Foundation
+
+struct Todo: Identifiable{ //struct now conforms to protocol called identifiable
+    
+    let id = UUID()
+    var title: String
+    var isCompleted: Bool = false //default value
+
+}
