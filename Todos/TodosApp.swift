95fa24c007837b515220dc5644edba7ae8de8672
Du Yuhan <yuhan.du@gmail.com>
2022-06-25 10:15:03 +0800

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Yuhan Du on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
